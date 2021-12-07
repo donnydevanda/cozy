de31fc0cc221ef5fb5bca992f87dedb30e762b9f
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-12-07 20:01:44 +0700

    Project Setup

---
+++ lib/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color purpleColor = Color(0xff5843BE);
+const Color orangeColor = Color(0xffFF9376);
+const Color blackColor = Color(0xff000000);
+const Color whiteColor = Color(0xffFFFFFF);
+const Color grayColor = Color(0xff82868E);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: blackColor
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+    fontWeight: FontWeight.w500,
+    color: whiteColor
+);
+
+TextStyle grayTextStyle = GoogleFonts.poppins(
+    fontWeight: FontWeight.w300,
+    color: grayColor
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+    fontWeight: FontWeight.w500,
+    color: purpleColor
+);
