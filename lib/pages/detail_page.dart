0b56c12175f26638b4338c7cef15b4c298d1f1be
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-12-08 14:57:47 +0700

    Detail Page - Image, Header

---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,139 @@
+import 'package:cozy/theme.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/thumbnail.png',
+              width: MediaQuery.of(context).size.width,
+              height: 350,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: Edge, vertical: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Image.asset(
+                      'assets/btn_back.png',
+                      width: 40,
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/btn_wishlist.png',
+                    width: 40,
+                  )
+                ],
+              ),
+            ),
+            ListView(
+              children: [
+                SizedBox(
+                  height: 328,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                    color: whiteColor,
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: Edge),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'KurrrrSS',
+                                  style: blackTextStyle.copyWith(fontSize: 22),
+                                ),
+                                SizedBox(
+                                  height: 2,
+                                ),
+                                Text.rich(
+                                  TextSpan(
+                                    text: '\$123',
+                                    style:
+                                        purpleTextStyle.copyWith(fontSize: 16),
+                                    children: [
+                                      TextSpan(
+                                          text: ' / month',
+                                          style: grayTextStyle.copyWith(
+                                              fontSize: 16))
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                  color: Color(0xff989BA1),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
